2f2fe7c05b534e1525ba7428cde835ac8c604205
Jada Crockett <jada.s.crockett@gmail.com>
2024-04-23 16:04:16 -0400

    Initial Commit

---
+++ WordMorphMadness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordMorphMadness
+//
+//  Created by Jada Crockett on 4/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordMorphMadness/WordMorphMadnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordMorphMadnessApp.swift
+//  WordMorphMadness
+//
+//  Created by Jada Crockett on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct WordMorphMadnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
