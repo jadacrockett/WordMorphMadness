4d346be0db3ff4ab909cecf54aed8e07bd6f2011
Jada Crockett <jada.s.crockett@gmail.com>
2024-04-29 19:25:45 -0600

    Created the methodology for Time Attack View to end with time (different times for different difficulties), and for the game to end once that time was up

--- WordMorphMadness/View/Game Play Views/Time Attack/TimeAttackView.swift
+++ WordMorphMadness/View/Game Play Views/Time Attack/TimeAttackView.swift
@@ -16,15 +16,28 @@ struct TimeAttackView: View {
     @State var repeatedWords: Int = 0
     @State var wrongWords: Int = 0
     private var difficulty: TAOptionView.Difficulty
-    
-    private var time: Int {
+    @State private var gameOver = false
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var timeElapsed = 0
+    private var timeLeft: Int {
         switch difficulty {
         case .easy:
-            return 90
+            return 60 - timeElapsed
         case .medium:
-            return 60
+            return 45 - timeElapsed
         case .hard:
-            return 30
+            return 30 - timeElapsed
+        }
+    }
+    
+    private var totalTime: DispatchTimeInterval {
+        switch difficulty {
+        case .easy:
+            return DispatchTimeInterval.seconds(60)
+        case .medium:
+            return DispatchTimeInterval.seconds(45)
+        case .hard:
+            return DispatchTimeInterval.seconds(30)
         }
     }
     
@@ -32,10 +45,14 @@ struct TimeAttackView: View {
 
         let wordView = LetterBoxRowFunctional(word: currWord, sucessfulWords: $sucessfulWords, repeatedWords: $repeatedWords, wrongWords: $wrongWords)
         ZStack {
-            Color.indigo2.ignoresSafeArea()
-            wordView
-                .padding(.top, 0.2)
-                
+            
+            if gameOver {
+                GameOverView(score: score)
+            } else {
+                Color.indigo2.ignoresSafeArea()
+                wordView
+                    .padding(.top, 0.2)
+            }
         }
         .onChange(of: sucessfulWords.count) {
             score += 100
@@ -58,9 +75,20 @@ struct TimeAttackView: View {
                     .font(.system(size: 25))
                     .foregroundStyle(Color.platinum)
             }
+            ToolbarItem(placement: .principal) {
+                Text("\(timeLeft)")
+                    .font(.system(size: 25))
+                    .foregroundStyle(Color.platinum)
+            }
             
         }
+        .onReceive(self.timer, perform: { time in
+            timeElapsed += 1
+        })
         .toolbar(.hidden, for: .tabBar)
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + self.totalTime, execute: {gameOver = true})
+        }
     }
     
     init(currWord: String, difficulty: TAOptionView.Difficulty) {
