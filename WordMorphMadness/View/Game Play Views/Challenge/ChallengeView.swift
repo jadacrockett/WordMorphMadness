00f669e0ecbca687309bdf9b2d1b7f96ec329955
Jada Crockett <jada.s.crockett@gmail.com>
2024-05-01 12:44:44 -0600

    Added text to ChallengeView to tell players what word they are trying to morph to

--- WordMorphMadness/View/Game Play Views/Challenge/ChallengeView.swift
+++ WordMorphMadness/View/Game Play Views/Challenge/ChallengeView.swift
@@ -25,13 +25,16 @@ struct ChallengeView: View {
                     Color.indigo2.ignoresSafeArea()
                     VStack {
                         LetterBoxRowFunctional(word: challengeLevelWordsByLevel[level], sucessfulWords: $sucessfulWords, repeatedWords: $repeatedWords, wrongWords: $wrongWords)
+                        Text("Get to \((challengeLevelDictionary[challengeLevelWordsByLevel[level]]?[0] as! String))")
+                            .font(.custom("Lovely Madness", size: 30))
+                            .foregroundStyle(Color.platinum)
+                        Spacer()
+                            .frame(height: 20)
                         NavigationLink(destination: ChallengeGameOver(level: level, letterSwaps: totalWords)) {
                             Text("Give Up!")
                                 .font(.custom("Lovely Madness", size: 30))
                         }.buttonStyle(GameOverButtonStyle())
                     }.onChange(of: sucessfulWords) {
-                        print(sucessfulWords.last!)
-                        print(challengeLevelDictionary[challengeLevelWordsByLevel[level]]?[0] as! String)
                         if sucessfulWords.last! == (challengeLevelDictionary[challengeLevelWordsByLevel[level]]?[0] as! String) {
                             gameOver = true
                         }
